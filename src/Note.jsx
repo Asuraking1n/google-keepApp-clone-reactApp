841193f8fbcbeb3a0be44f40b7f06a1a234741af
Nishant Kumar Tiwari <nishant99tiwari@gmail.com>
2021-07-19 03:07:35 -0700

    Update Note.jsx

--- src/Note.jsx
+++ src/Note.jsx
@@ -3,8 +3,6 @@
 import React from "react";
 import "./index.css";
 import DeleteIcon from '@material-ui/icons/Delete';
-import { PinDropSharp } from "@material-ui/icons";
-
 
 function Note(props) {
     const deleteNote=()=>{
